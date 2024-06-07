a22c0bc38076643736c8b48d9dec51254b48f3ce
kushan_105754 <kushan.tharaka@axiatadigitallabs.com>
2024-06-07 14:14:25 +0530

    updated the calculator feature

--- Program.cs
+++ Program.cs
@@ -1,9 +1,28 @@
 ﻿using System;
 
+
+// Exercise making a calculator that can add, subtract, multiply, and divide
+
 public class Program{
     public static void Main(String[] args){
-        Console.WriteLine("Enter your name:");
-        String name = Console.ReadLine();
-        Console.WriteLine("Hello " + name);
+        String exitKey= null;
+        Console.WriteLine("Calculator : Two Number Version \n add First Number:");
+        int firstNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("add Second Number:");
+        int secondNumber = int.Parse(Console.ReadLine());
+        
+        while (exitKey != "e" || exitKey != "E"){
+        Console.WriteLine("Add the Operation need to Perform \n 1: ADD \n 2: Substraction \n 3: Multiply \n 4: Division \n For exit press e or E " );
+        int operation = int.Parse(Console.ReadLine());
+        if(operation == 1){
+            Console.WriteLine("The Result is " + (firstNumber + secondNumber));
+        }else if(operation == 2){
+            Console.WriteLine("The Result is "+( firstNumber-secondNumber));
+        }else if(operation == 3){
+            Console.WriteLine("The Result is "+( firstNumber*secondNumber));
+        }else if(operation == 4){
+            Console.WriteLine("The Result is "+( firstNumber/secondNumber));
+        }}
+        
     }
 }
